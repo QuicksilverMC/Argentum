c9994036597e1403db0fc09f0d3da3da76cb9c58
rdh <me@rdh540.dev>
2026-09-14 03:49:11 -0400

    nvidia threaded optimization workarounds

--- src/main/java/dev/rdh/argentum/mixin/core/MinecraftMixin.java
+++ src/main/java/dev/rdh/argentum/mixin/core/MinecraftMixin.java
@@ -11,6 +11,7 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import dev.rdh.argentum.impl.Argentum;
+import dev.rdh.argentum.impl.compat.NvidiaWorkarounds;
 import dev.rdh.argentum.impl.render.hud.item.GuiItemIcons;
 
 @Mixin(Minecraft.class)
@@ -20,6 +21,16 @@ public abstract class MinecraftMixin {
 
     @Shadow
     private boolean logGlErrors;
+
+    @Inject(method = "initDisplay", at = @At("HEAD"))
+    private void argentum$installNvidiaWorkarounds(CallbackInfo ci) {
+        NvidiaWorkarounds.install();
+    }
+
+    @Inject(method = "initDisplay", at = @At("RETURN"))
+    private void argentum$uninstallNvidiaWorkarounds(CallbackInfo ci) {
+        NvidiaWorkarounds.uninstall();
+    }
 
     @Inject(method = "init", at = @At("RETURN"))
     private void argentum$configureGlErrorChecking(CallbackInfo ci) {
